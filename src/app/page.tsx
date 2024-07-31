21a3ec3e3c1123208ee2ed27a18ea1ff075495bb
bkctrl <hb3kang@uwaterloo.ca>
2024-07-31 14:46:18 +0900

    Finalized design

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import Image from "next/image";
+import { App } from "@/components/App"
+
+export default function Home() {
+  return (
+      <div>
+        <App />
+      </div>
+    );
+}
